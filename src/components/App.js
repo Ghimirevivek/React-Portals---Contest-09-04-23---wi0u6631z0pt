dc0d64d796dde4c0214325a0da6f9fb27344276f
Ghimirevivek <stephengmre@gmail.com>
2023-04-09 20:34:11 +0530

    me

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,32 @@
-import React ,{useState, useRef} from 'react'
-import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
+import React, { useState, useRef } from 'react'
+import '../styles/App.css'
+import PortalButton from './PortalButton.js'
+import PortalTextArea from './PortalTextArea.js'
 
-//complete the function 
+//complete the function
 const App = () => {
+  const [text, setText] = useState('')
 
+  const handleInputChange = (event) => {
+    setText(event.target.value)
+  }
 
+  const handleButtonClick = () => {
+    ReactDOM.render(
+      <PortalTextArea value={text} />,
+      document.getElementById('portal-textfield')
+    )
+  }
 
   return (
-    
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input id="input" value={text} onChange={handleInputChange}></input>
+      <br />
+      <PortalButton buttonclick={handleButtonClick} />
+      <br />
+      <PortalTextArea value={text} />
     </div>
   )
 }
 
-
-export default App;
+export default App
--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 //complete this function using portals
-const PortalButton=()=>{
-    return (
-        <div>
-            <button id="button" onClick={}>Click</button>
-        </div>
-        
-    )
+const PortalButton = ({ buttonclick }) => {
+  const handleClick = () => {
+    buttonclick()
+  }
+
+  return ReactDOM.createPortal(
+    <div>
+      <button id="button" onClick={handleClick}>
+        Click
+      </button>
+    </div>,
+    document.getElementById('portal-button')
+  )
 }
-export default PortalButton;
+export default PortalButton
 
 //portal-button id will be used here for portal purpose
